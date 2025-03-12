da924824dbe1745d4c5e81accc94ceeec659b449
Miftaqul Fiqi Firmansyah <miftaqulfiqifirmansyah131@gmail.com>
2025-03-13 00:33:18 +0700

    update react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function message() {
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080"); // Sesuaikan dengan alamat server
+
+    ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      setMessage(data);
+    };
+
+    return () => ws.close();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Live Sensor Data</h1>
+      {message ? <p>Message: {message}</p> : <p>Waiting for data...</p>}
+    </div>
+  );
 }
-
-export default App
