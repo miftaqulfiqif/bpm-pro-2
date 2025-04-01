49503fc7b1507b5725e7de8cddaca5d92c3e3a6d
Miftaqul Fiqi Firmansyah <miftaqulfiqifirmansyah131@gmail.com>
2025-04-01 20:44:55 +0700

    change mysql database

--- backend/prisma/migrations/20250325085804_modify_category_results_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `gender` on the `category_results` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "category_results" DROP COLUMN "gender";
